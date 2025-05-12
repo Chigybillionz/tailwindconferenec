d86291864aaf06204f582f6cbefe6c902f18b6c1
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2025-05-12 12:47:56 +0100

    mondy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,48 @@
+// module.exports = {
+//   mode: 'jit', // Enable Just-In-Time mode for faster builds
+//   content: [
+//     './dist/**/*.html', // Include all HTML files in the dist folder
+//     './dist/**/*.js',   // Include all JS files in the dist folder
+//   ],
+//     theme: {
+//         screens: {
+//         'sm': '480px',
+//         'md': '768px',
+//         'lg': '1024px',
+//         'xl': '1280px',
+        
+//         },
+//                 backgroundImage: {
+//                   // 'mobile': "url('images/background-mobile.png')",
+//                   'mobile': "bg-gradient-to-b from-[#1E1E2F] to-[#1E1E2F]",
+//                   'desktop': "url('images/background-desktop.png')",
+//                 },
+//               },
+//         }
+      
 module.exports = {
   mode: 'jit', // Enable Just-In-Time mode for faster builds
   content: [
     './dist/**/*.html', // Include all HTML files in the dist folder
     './dist/**/*.js',   // Include all JS files in the dist folder
   ],
-    theme: {
-        screens: {
-        'sm': '480px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        
-        },
-                backgroundImage: {
-                  // 'mobile': "url('images/background-mobile.png')",
-                  'mobile': "bg-gradient-to-b from-[#1E1E2F] to-[#1E1E2F]",
-                  'desktop': "url('images/background-desktop.png')",
-                },
-              },
-        }
-      
+  theme: {
+    screens: {
+      'xs': '320px', // Custom mobile size for very small devices
+      'sm': '480px', // Small devices (default mobile size)
+      'md': '768px', // Medium devices (tablets)
+      'lg': '1024px', // Large devices (desktops)
+      'xl': '1280px', // Extra large devices
+    },
+    extend: {
+      backgroundImage: {
+        'mobile': "url('images/background-mobile.png')",
+
+        'desktop': "url('images/background-desktop.png')",
+      },
+    },
+  },
+  plugins: [],
+};
     
   
