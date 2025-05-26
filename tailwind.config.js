c01104107a2f79d4fd019172d9a39264d5c95f8d
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2025-05-26 16:12:56 +0100

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,4 @@
-// module.exports = {
-//   mode: 'jit', // Enable Just-In-Time mode for faster builds
-//   content: [
-//     './dist/**/*.html', // Include all HTML files in the dist folder
-//     './dist/**/*.js',   // Include all JS files in the dist folder
-//   ],
-//     theme: {
-//         screens: {
-//         'sm': '480px',
-//         'md': '768px',
-//         'lg': '1024px',
-//         'xl': '1280px',
-        
-//         },
-//                 backgroundImage: {
-//                   // 'mobile': "url('images/background-mobile.png')",
-//                   'mobile': "bg-gradient-to-b from-[#1E1E2F] to-[#1E1E2F]",
-//                   'desktop': "url('images/background-desktop.png')",
-//                 },
-//               },
-//         }
-      
+
 module.exports = {
   mode: 'jit', // Enable Just-In-Time mode for faster builds
   content: [
